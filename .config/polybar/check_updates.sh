bd7fb2cc41a36f12a7d5d6708c8dab73d347ee03
Aditya Suresh <153178583+AdityaSuresh013@users.noreply.github.com>
2024-10-18 22:05:06 +0530

    add check_updates.sh

---
+++ .config/polybar/check_updates.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check for package updates
+updates=$(apt list --upgradable 2>/dev/null | grep -v 'Listing...' | wc -l)
+
+# If there are updates, show the number
+if [ "$updates" -gt 0 ]; then
+    echo "Updates: $updates"
+#else
+    #echo "No updates"
+fi
+
